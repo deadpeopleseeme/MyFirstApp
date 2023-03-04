5246b99f8bb3537631523bae04b32b974b93d209
deadpeopleseeme <volkikushaytnogi@gmail.com>
2023-03-04 21:08:17 +0100

    Добавьте файлы проекта.

---
+++ MyFirstApp.ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        const string MyName = "Val";
+
+        Console.WriteLine(MyName);
+
+        Console.WriteLine("\t Hello, world!");
+        Console.WriteLine("\t I'm 34 y.o");
+        Console.WriteLine("\t My name is \n {0}", MyName);
+        Console.WriteLine("\u0040");
+        Console.WriteLine("\x23");
+        Console.ReadKey();
+
+    }
+}
