0833c8ceacf17f1e52a834bcec3d7a8630c82b09
deadpeopleseeme <volkikushaytnogi@gmail.com>
2023-03-10 13:45:36 +0100

    from if-else to switch, added cyan color

--- MyFirstApp.ConsoleApp/Program.cs
+++ MyFirstApp.ConsoleApp/Program.cs
@@ -9,28 +9,30 @@ class MainClass
         string color = Console.ReadLine();
         color = color.ToLower();
 
-        if (color == "red")
+        switch (color) 
         {
-            Console.BackgroundColor = ConsoleColor.Red;
-            Console.ForegroundColor = ConsoleColor.Black;
-
-            Console.WriteLine("Your color is red!");
+            case "red":
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Your color is red!");
+                break;
+            case "green":
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Your color is green!");
+                break;
+            case "cyan":
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Your color is cyan!");
+                break;
+            default:
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Well man that's default case, no color for you blah blah blah :D");
+                break;
         }
-
-        else if (color == "green")
-        {
-            Console.BackgroundColor = ConsoleColor.Green;
-            Console.ForegroundColor = ConsoleColor.Black;
-
-            Console.WriteLine("Your color is green!");
-        }
-        else
-        {
-            Console.BackgroundColor = ConsoleColor.Cyan;
-            Console.ForegroundColor = ConsoleColor.Black;
-
-            Console.WriteLine("Your color is cyan!");
-        }
+        
         Console.WriteLine("press any key to exit...");
         Console.ReadKey();
     }
