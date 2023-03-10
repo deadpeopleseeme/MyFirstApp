5e38923138c91d3dbed611760e727a89cdda0db0
deadpeopleseeme <volkikushaytnogi@gmail.com>
2023-03-10 13:33:22 +0100

    4.1.17 (your favorite color)

--- MyFirstApp.ConsoleApp/Program.cs
+++ MyFirstApp.ConsoleApp/Program.cs
@@ -4,17 +4,40 @@ class MainClass
 {
     public static void Main(string[] args)
     {
+        Console.WriteLine("Напишите свой любимый цвет на английском: ");
 
-        const string MyName = "Val";
+        string color = Console.ReadLine();
+        color = color.ToLower();
 
-        Console.WriteLine(MyName);
+        if (color == "red")
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
 
-        Console.WriteLine("\t Hello, world!");
-        Console.WriteLine("\t I'm 34 y.o");
-        Console.WriteLine("\t My name is \n {0}", MyName);
-        Console.WriteLine("\u0040");
-        Console.WriteLine("\x23");
+            Console.WriteLine("Your color is red!");
+        }
+
+        else if (color == "green")
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is green!");
+        }
+        else
+        {
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is cyan!");
+        }
+        Console.WriteLine("press any key to exit...");
         Console.ReadKey();
-
     }
+
+    
+
 }
+
+
+
